3faae7468a240a88aa58a0745d48772948c02122
William Jensen <williambjensen01@gmail.com>
2020-08-02 20:01:07 -0600

    Stretch 2

--- index.js
+++ index.js
@@ -350,11 +350,20 @@ function getHTML(/* Code here */){
 
 /* STRETCH 2: Create a function called `randomize` that takes a data array as an argument and returns a the same array in a randomized order. */
 
-function randomize(/* Code here */){
+function randomize(array){
+  let ctr = array.length;
+  let temp;
+  let index;
 
-    /* Code here */
-
+  while (ctr > 0) {
+    index = Math.floor(Math.random() * ctr);
+    ctr--;
+    temp = array[ctr];
+    array[ctr] = array[index];
+    array[index] = temp;
   }
-
+  console.log("Stretch 2: ", array)
+}
+ randomize(artists);
 
  /* STRETCH 3: Use advanced array methods (.map, .reduce, .filer) to refactor your MVP code (create an array of all artists born in the 1900s with .filter, for example) */
